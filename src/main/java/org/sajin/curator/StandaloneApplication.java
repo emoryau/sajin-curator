7d1842686dd6df9987a0bd0a6e2896376cc3015e
emoryau <emoryau@gmail.com>
2019-03-12 13:13:53 -0400

    Added basic spring security deps & config

--- src/main/java/org/sajin/curator/StandaloneApplication.java
+++ src/main/java/org/sajin/curator/StandaloneApplication.java
@@ -1,8 +1,10 @@
 package org.sajin.curator;
 
+import org.sajin.curator.spring.config.OAuth2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author emory.au
  */
 @SpringBootApplication
+@Import(OAuth2Config.class)
 public class StandaloneApplication {
 
 	@Bean
